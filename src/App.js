30debe0dde96386db59cc6494c9fd60a323c7f06
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2022-10-23 06:39:47 -0400

    Created clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App()
+{
+  const name = 'Raul Camargo';
+  const element1 = <h1>Hello, {name}</h1>;
+
+  // function formatName(user)
+  // {
+  //   return user.firstName + user.lastName;
+  // }
+
+
+  // const user = 
+  // {
+  //   firstName: 'Harper',
+  //   lastName: 'Perez'
+  // };
+
+  // const element2 = (
+  //   <h1>
+  //     Hello, {formatName(user)}!
+  //   </h1>
+  // );
+
+  return(
+    <div className='App'>
+      {element1}
     </div>
   );
 }
